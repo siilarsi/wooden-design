ad3a7cb78def064a0b1e32d4bccf8b1790308a39
Siilarsi Lorenzen <siilarsi@gmail.com>
2025-06-29 00:04:53 +0200

    Fix viewer imports and add event log

--- tests/viewer.test.js
+++ tests/viewer.test.js
@@ -6,4 +6,8 @@ describe('viewer container', () => {
   it('contains #viewer element', () => {
     expect(/<div id="viewer">/.test(html)).toEqual(true);
   });
+
+  it('contains #eventLog element', () => {
+    expect(/<div id="eventLog">/.test(html)).toEqual(true);
+  });
 });
