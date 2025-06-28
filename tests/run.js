09b73088a07971c4fbfb6d554fa1a2dc2fe736cc
Siilarsi Lorenzen <siilarsi@gmail.com>
2025-06-28 23:34:40 +0200

    Add syntax linting

--- tests/run.js
+++ tests/run.js
@@ -3,5 +3,6 @@ import './utils.test.js';
 import './gltf.test.js';
 import './viewer.test.js';
 import './instructions.test.js';
+import './syntax.test.js';
 import { run } from './test-utils.js';
 await run();
